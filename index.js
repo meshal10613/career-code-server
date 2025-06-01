a637afb56917f380b8e743beaa951f134f6236f6
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-06-01 11:54:06 +0600

    jwt token and firebase token authorization done

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.port ||3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+var admin = require("firebase-admin");
+var serviceAccount = require("./firebase-admin-key.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const logger = (req, res, next) => {
+    console.log("Inside the logger middleware");
+    next();
+};
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    // console.log('cookie', token);
+    if(!token){
+        return res.status(401).send({message: 'unauthorized access'});
+    };
+    //verify token
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        // console.log(decoded)
+        res.decoded = decoded;
+        next();
+    })
+};
+
+const verifyFirebaseToken = async(req, res, next) => {
+    const token = req?.headers?.authorization
+    // console.log("firebase token",token);
+    if(!token){
+        return res.status(401).send({message: "unauthorized access"})
+    }
+    const userInfo = await admin.auth().verifyIdToken(token);
+    req.tokenEmail = userInfo.email;
+    next();
+};
+
 
 app.get('/', (req, res) => {
-    res.send('Server is running....')
+    res.send('Server is running....');
+    next();
 });
 
 
@@ -33,9 +80,52 @@ async function run() {
         const jobsCollection = client.db("careerCode").collection("jobs");
         const applicationCollection = client.db("careerCode").collection("applications");
 
+        //jwt token related api
+        app.post("/jwt", async(req, res) => {
+            const {email} = req.body;
+            const user = {email};
+            const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: "10d"});
+
+            //set token in the cookies
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false,
+
+            })
+
+            res.send(token);
+        });
+
         //jobs
         app.get("/jobs", async(req, res) => {
-            const result = await jobsCollection.find().toArray();
+            const email = req.query.email;
+            let query = {};
+            if(email){
+                query = {
+                    hr_email: email
+                }
+            };
+
+            const result = await jobsCollection.find(query).toArray();
+            res.send(result);
+        });
+        
+        app.get("/jobs/applications", async(req, res) => {
+            const email = req.query.email;
+            const query = {
+                hr_email: email
+            };
+            const result = await jobsCollection.find(query).toArray();
+
+            //should use aggregate
+            for(const job of result){
+                const applicationQuery = {
+                    jobId: job._id.toString()
+                };
+                const applicationCount = await applicationCollection.countDocuments(applicationQuery);
+                job.applicationCount = applicationCount;
+            }
+
             res.send(result);
         });
 
@@ -48,15 +138,26 @@ async function run() {
             res.send(result);
         });
 
+        app.post("/jobs", async(req, res) => {
+            const jobs = req.body;
+            const result = await jobsCollection.insertOne(jobs);
+            res.send(result);
+        })
+
         //applications
-        app.get("/applications", async(req, res) => {
+        app.get("/applications", logger, verifyToken, verifyFirebaseToken, async(req, res) => {
             const email = req.query.email;
+
+            if(email !== req.tokenEmail){
+                return res.status(403).send({message: "forbiden access"})
+            };
+
             let query = {};
             if(email){
                 query = {
                     applicant: email
                 };
-            }
+            };
             const result = await applicationCollection.find(query).toArray();
 
             for(const application of result){
@@ -71,9 +172,28 @@ async function run() {
             res.send(result);
         });
 
+        app.get("/applications/job/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = {jobId: id};
+            const result = await applicationCollection.find(query).toArray();
+            res.send(result);
+        });
+
         app.post("/applications", async(req, res) => {
             const application = req.body;
             const result = await applicationCollection.insertOne(application);
+            res.send(result);
+        });
+
+        app.patch("/applications/:id", async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const updatedDoc = {
+                $set: {
+                    status: req.body.status
+                }
+            };
+            const result = await applicationCollection.updateOne(filter, updatedDoc);
             res.send(result);
         });
 
